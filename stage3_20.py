b839d5c7349d6afcb67694a36eedc93d3dc36bd9
kuni <kuni@gmail.com>
2015-03-24 00:23:46 +0900

    NLP100 Problem 20: read json file

---
+++ stage3_20.py
@@ -0,0 +1,26 @@
+#coding: utf-8
+#NLP100
+#http://www.cl.ecei.tohoku.ac.jp/nlp100/
+
+import sys
+import json
+import codecs
+
+#sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+
+#read file
+file = 'jawiki-country.json'
+f = open(file, 'r')
+
+#イギリスが出るまでfileを読み続ける
+while True:
+    str_f = f.readline()
+    if str_f=="":
+        break
+    jsondata = json.loads(str_f)
+    if jsondata['title']==u'イギリス':
+        #text部分を書き出す
+        print jsondata['text']
+        break
+
+
